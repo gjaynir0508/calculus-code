b08bbad93db457be8aba1268257c404b30a2d455
Jayanth Gudimella <gjaynir0508@gmail.com>
2022-10-22 19:42:57 +0530

    Add basic Elements to MainLayout.tsx

---
+++ components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function Footer() {
+	return <footer>Footer</footer>;
+}
+
+export default Footer;
--- layouts/MainLayout/index.tsx
+++ layouts/MainLayout/index.tsx
@@ -1,1 +1,13 @@
+import Footer from "@components/Footer";
+import Navbar from "@components/Navbar";
 import * as React from "react";
+
+function MainLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<Navbar />
+			<main>{children}</main>
+			<Footer />
+		</>
+	);
+}
