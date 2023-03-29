6775cf69d8aced5f40b4af902460171ffbc48fae
Jayanth Gudimella <gjaynir0508@gmail.com>
2023-03-29 15:40:15 +0530

    Add Google Ad Sense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script
+				key="google-ads"
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1807532107669436"
+				crossOrigin="anonymous"
+			></Script>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
+
